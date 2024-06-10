35556f66185d512f99cf0260f86a6e5573a3e089
marioliveira-mbi <mariana.oliveira@mb.com.br>
2024-06-10 15:30:34 -0300

    feat: implementing select and tests in UsersRepository

--- src/infra/db/entities/users.py
+++ src/infra/db/entities/users.py
@@ -10,8 +10,5 @@ class Users(Base):
     age = Column(Integer, nullable=False)
 
     def __repr__(self):
-        return f"""Users [id={self.id},
-                first_name={self.first_name}, 
-                last_name={self.last_name}, 
-                age={self.age}]"""
+        return f"""Users [id={self.id}, first_name={self.first_name}"""
     
