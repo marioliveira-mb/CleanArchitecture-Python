8c1e01b2bccbe1f810bd47777bfef363e6752be9
marioliveira-mbi <mariana.oliveira@mb.com.br>
2024-06-11 13:35:38 -0300

    feat: Starting implementing use_cases

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -1,7 +1,9 @@
+from typing import List
 from src.infra.db.settings.connection import DBConnectionHandler
 from src.infra.db.entities.users import Users as UsersEntity
-
-class UsersRepository:
+from src.data.interfaces.users_repository import UsersRepositoryInterface
+from src.domain.models.users import Users
+class UsersRepository(UsersRepositoryInterface):
 
     @classmethod
     def insert_user(cls, first_name: str, last_name: str, age: int) -> None:
@@ -19,7 +21,7 @@ class UsersRepository:
                 raise exception
 
     @classmethod
-    def select_user(cls, first_name: str) -> any:
+    def select_user(cls, first_name: str) -> List[Users]:
         with DBConnectionHandler() as database:
             try:
                 users = (
