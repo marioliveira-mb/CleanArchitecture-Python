2724c40a60e68701fef3557f8ca1f57611d91d10
marioliveira-mbi <mariana.oliveira@mb.com.br>
2024-06-14 11:29:50 -0300

    feat: Implementing user_register

---
+++ src/data/use_cases/user_register_test.py
@@ -0,0 +1,49 @@
+from src.infra.db.tests.users_repository import UsersRepositorySpy
+from .user_register import UserRegister
+
+def test_register():
+    first_name = 'ola'
+    last_name = 'aqui'
+    age = 3
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    response = user_register.register(first_name, last_name, age)
+
+    print(repo.insert_user_attributes)
+
+    assert response['type'] == 'Users'
+    assert response['count'] == 1
+    assert response['attributes'] == {"first_name": first_name, "last_name": last_name, "age": age}
+
+def test_register_error_name_with_number():
+    first_name = 'ola123'
+    last_name = 'aqui'
+    age = 3
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    print(repo.insert_user_attributes)
+
+    try:
+        user_register.register(first_name, last_name, age)
+    except Exception as e:
+        assert str(e) == "Nome inválido para busca"
+
+
+def test_register_error_in_long_name():
+    first_name = 'olahujhfljksdahljkfhjksafhjsdafhjklhjsad'
+    last_name = 'aqui'
+    age = 3
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    print(repo.insert_user_attributes)
+
+    try:
+        user_register.register(first_name, last_name, age)
+    except Exception as e:
+        assert str(e) == "Nome muito grande para busca"
